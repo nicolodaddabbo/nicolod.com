feb8420b036823df8ac815495d9ee1f65dd4d305
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2025-05-13 20:03:19 +0200

    firefox FOUC and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nicolod.com",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   },
